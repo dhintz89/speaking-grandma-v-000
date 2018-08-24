e1ef44cb2230114ea32ec3423fb8843511f8e633
Dan Hintz <dhintz@live.com>
2018-08-24 00:41:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase = phrase.upcase
-    if phrase = "I LOVE YOU GRANDMA!"
+  if phrase == phrase.upcase
+    if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
     else
       return "NO, NOT SINCE 1938!"
